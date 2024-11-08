44da4d1976c86766f81dd3a55598c9cbe96550e1
isayantan <sayantan.choudhury4@gmail.com>
2024-11-07 20:29:22 -0500

    Updated.

--- yolo/__init__.py
+++ yolo/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask_cors import CORS
 def create_app(test_config=None):
-    app = Flask("Ultralytics", instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True)
 
     # Register blueprints
     from . import database, classifier
